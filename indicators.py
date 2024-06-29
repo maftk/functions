7efb4979c9713ac37d82f2dcdab136e3853322d5
root <root@24d34a733f73>
2024-06-29 12:38:07 +0000

    mod df and delete ax in kwargs

--- indicators.py
+++ indicators.py
@@ -1,8 +1,7 @@
-
 import numpy as np
 import pandas as pd
 
-def sma(df=None,ax=None,sw=5,lw=25):
+def sma(df=None,sw=5,lw=25):
   s = f'sma{sw}'
   l = f'sma{lw}'
   df[s] = df['Close'].rolling(window=sw,min_periods=1).mean()
@@ -13,16 +12,15 @@ def sma(df=None,ax=None,sw=5,lw=25):
   df['smapos'] = np.concatenate(([0], np.diff(smac)))
   return df
 ##指数平滑移動平均
-def ema(df=None,ax=None,sw=5,lw=25):
+def ema(df=None,sw=5,lw=25):
   sema = f'ema{sw}'
   lema = f'ema{lw}'
   df[sema] = df['Close'].ewm(span=sw,).mean()
   df[lema] = df['Close'].ewm(span=lw,).mean()
   df['emapos'] = np.concatenate(([0],np.diff(np.where(df[sema] > df[lema], 1, 0))))
   return df
-  # バックテストの結果を出力
 ##移動平均収束拡散法
-def macd(df=None,sw=12,lw=26,sig=9,ax=None):
+def macd(df=None,sw=12,lw=26,sig=9):
   sw = f'ema{sw}'
   lw = f'ema{lw}'
   df = ''.ema(df=df,ax=ax,sw=12,lw=26)
@@ -35,7 +33,7 @@ def macd(df=None,sw=12,lw=26,sig=9,ax=None):
   return df
 #ボラティリティ系
 ##ボリンジャーバンド
-def bband(df=None,ax=None,n=25):
+def bband(df=None,n=25):
   # N期間の移動平均線(SMA)を計算
   df['sma'] = df['Close'].rolling(window=n).mean()
   # 上部バンドを計算
@@ -48,7 +46,7 @@ def bband(df=None,ax=None,n=25):
   bbsel = np.where(df['Close'] > df['ub'], -1, 0)
   df['bbpos'] = bbbuy + bbsel
   return df
-def rsi(w=14,df=None,ax=None):
+def rsi(df=None,w=12):
   # w = 12
   delta = df['Close'].diff()
   gain = (delta.where(delta > 0,0)).rolling(w).mean()
@@ -59,7 +57,7 @@ def rsi(w=14,df=None,ax=None):
   df.loc[df['Close'].isna(),'rsi'] = np.nan
   df['rsipos'] = np.where(df['rsi'] > 70, -1, np.where(df['rsi'] < 30, 1, 0))
   return df
-def fkd(df=None,ax=None):
+def fkd(df=None):
   # ロープライスを計算
   llow = df['Low'].rolling(window=14).min()
   # ハイプライスを計算
@@ -75,7 +73,7 @@ def fkd(df=None,ax=None):
   # df.loc[(df['%k'] > df['%d']), 'fkdpos'] = 1
   # df.loc[(df['%k'] < df['%d']), 'fkdpos'] = -1
   return df
-def dmi(df=None,ax=None):
+def dmi(df=None):
   n=14
   # True Range (TR) の計算
   tr = np.maximum(df['High'] - df['Low'],  np.abs(df['High'] - df['Close'].shift(1)),  np.abs(df['Low'] - df['Close'].shift(1)))
@@ -95,7 +93,7 @@ def dmi(df=None,ax=None):
   df.loc[df['Close'].isna(),['+di','-di','adx']] = np.nan
   df['dmipos'] = np.concatenate(([0], np.diff(np.where(df['+di'] > df['-di'], 1, 0))))
   return df
-def cloud(df=None,ax=None):
+def cloud(df=None):
   high9 = df['High'].rolling(window=9).max()
   low9 = df['Low'].rolling(window=9).min()
   df['hlm9'] = ((high9 + low9) / 2)
